f74d3f103168a2e135738a8bdd6a9ab55c3433a4
athul-04 <athulrobertvit@gmail.com>
2024-12-02 20:40:09 +0530

    First Commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args) {
+        int i = 0;       // Initialized but not used
+        short j = 0;     // Initialized but not used
+        byte k = 0;      // Corrected initialization
+
+        String s = "";   // Empty string initialized
+        System.out.println(s); // Prints an empty line
+    }
+}
